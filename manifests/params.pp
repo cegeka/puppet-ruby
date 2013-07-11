puppetlabs-toy-chest/puppetlabs-ruby

lint fixes and add rubygems_package param

manifests/params.pp
@@ -2,35 +2,24 @@
 #
 # This class handles the Ruby module parameters
 #
-# Parameters:
-#   $ruby_dev = the name of the Ruby development libraries
-# 
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
 class ruby::params {
-
   $version          = 'installed'
   $gems_version     = 'installed'
   $ruby_package     = 'ruby'
+  $rubygems_package = 'rubygems'
 
-  case $osfamily {
-    "redhat", "suse": {
-      $ruby_dev="ruby-devel"
-      $rubygems_update  = true
+  case $::osfamily {
+    'redhat', 'amazon': {
+      $ruby_dev = 'ruby-devel'
+      $rubygems_update = true
     }
-    "debian": {
-      $ruby_dev= [ "ruby-dev", "rake", "irb" ]
-      $rubygems_update  = false
+    'debian': {
+      $ruby_dev = [ 'ruby-dev', 'rake', 'irb' ]
+      $rubygems_update = false
     }
-
     default: {
       fail("Unsupported OS family: ${::osfamily}")
     }
   }
 
 }
-

cegeka/puppet-ruby

manifests/params.pp
@@ -2,35 +2,24 @@
 #
 # This class handles the Ruby module parameters
 #
-# Parameters:
-#   $ruby_dev = the name of the Ruby development libraries
-#
-# Actions:
-#
-# Requires:
-#
-# Sample Usage:
-#
 class ruby::params {
-
   $version          = 'installed'
   $gems_version     = 'installed'
   $ruby_package     = 'ruby'
+  $rubygems_package = 'rubygems'
 
-  case $osfamily {
-    "redhat", "suse": {
-      $ruby_dev="ruby-devel"
-      $rubygems_update  = true
+  case $::osfamily {
+    'redhat', 'amazon': {
+      $ruby_dev = 'ruby-devel'
+      $rubygems_update = true
     }
-    "debian": {
-      $ruby_dev= [ "ruby-dev", "rake", "ri" ]
-      $rubygems_update  = false
+    'debian': {
+      $ruby_dev = [ 'ruby-dev', 'rake', 'ri' ]
+      $rubygems_update = false
     }
-
     default: {
       fail("Unsupported OS family: ${::osfamily}")
     }
   }
 
 }
-